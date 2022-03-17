93b6c153ae7c959f935704f3319a21bf4aa54836
Tt421 <93419491+Tt421@users.noreply.github.com>
2022-03-17 19:31:01 +0800

    Update main.cc

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -16,28 +16,44 @@ int64_t now()
 int main(int argc, char* argv[])
 {
   init_neighbors();
-
-  FILE* fp = fopen(argv[1], "r");
+  //输入文件名
+  char str[20];
+  scanf("%s", str);
+  int len1 = strlen(str);
+  char* t;
+  if(len1 == 10)
+  	{t ="test_group";}
+  
+  FILE* fp = fopen(t, "r");
+  char filename[128];
+  char* f;
   char puzzle[128];
   int total_solved = 0;
   int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
-      solve = solve_sudoku_min_arity;
-    else if (argv[2][0] == 'c')
-      solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
+  
+  bool (*solve)(int) = solve_sudoku_dancing_links;
+  
   int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-    if (strlen(puzzle) >= N) {
+  
+  while (fgets(filename, sizeof filename, fp) != NULL) {
+
+  int len = strlen(filename);
+  if(len == 8)
+  	f = "test1";
+  else if(len == 11)
+  	f = "test1000";
+  else
+  	f = "test10000";
+  FILE* fp2 = fopen(f, "r");
+  total_solved = 0;
+  total = 0;
+  
+  while (fgets(puzzle, sizeof puzzle, fp2) != NULL) {
+     if (strlen(puzzle) >= N) {
       ++total;
       input(puzzle);
       init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
+
       if (solve(0)) {
         ++total_solved;
         if (!solved())
@@ -47,11 +63,20 @@ int main(int argc, char* argv[])
         printf("No: %s", puzzle);
       }
     }
+    
+    for(int i = 0 ; i<81 ; i++)
+    	printf("%d",board[i]);
+    printf("\n");
+    
   }
+  /*
+  //时间
   int64_t end = now();
   double sec = (end-start)/1000000.0;
   printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
-
+  */
+  }
+  
   return 0;
 }
 
