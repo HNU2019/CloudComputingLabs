94651a80b87d089b7f668611d4c4aadd4f0fee1b
Hubert <1207820441@qq.com>
2022-03-21 20:06:15 +0800

    suppress warnings

--- Lab1/src/Sudoku/sudoku/Main.java
+++ Lab1/src/Sudoku/sudoku/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
+@SuppressWarnings("unchecked")
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
