7de614a52bf655fe25420e4853752588faf38f8d
Hubert <1207820441@qq.com>
2022-04-12 21:40:33 +0800

    can read the long option

--- Lab2/src/server/Main.java
+++ Lab2/src/server/Main.java
@@ -5,23 +5,42 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         int port = 8080;
+        String serverIp = "0.0.0.0";
+        int coresNum = Runtime.getRuntime().availableProcessors(); //获取cpu核心数
+        int threadNum = coresNum*2;  //线程数
+
 //        for(String s:args) System.out.println(s);
-        if (args.length >= 2 && args[0].equals("-p")) {
-            try {
-                port = Integer.parseInt(args[1]);
-                if (port < 1024 || port > 49151) {
-                    System.out.println(port + " is not a valid port number!");
-                    System.out.println("port must be set in the range of [1024,49151]");
-                    System.exit(1);
+        for (int i = 0; i < args.length - 1; i += 2) {
+            if (args[i].equals("-i") || args[i].equals("--ip")) {
+                serverIp = args[i + 1];
+            } else if (args[i].equals("-p") || args[i].equals("--port")) {
+                try {
+                    port = Integer.parseInt(args[i + 1]);
+                    if (port < 1024 || port > 49151) {
+                        System.out.println(port + " is not a valid port number!");
+                        System.out.println("port must be set in the range of [1024,49151]");
+                        System.exit(-1);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("please enter the correct port");
+                    System.exit(-1);
                 }
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                System.out.println("please enter the correct port");
-                System.exit(1);
+            } else if (args[i].equals("-t") || args[i].equals("--threads")) {
+                try {
+                    threadNum = Integer.parseInt(args[i + 1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("please enter the correct number of threads");
+                    System.exit(-1);
+                }
             }
         }
+        //用来检查输入参数
+//        System.out.println(serverIp);
+//        System.out.println(port);
+//        System.out.println(threadNum);
+
         // 创建serverThread 实例
-        Server server = new Server(port);
+        Server server = new Server(serverIp,port);
         server.Start();
     }
 }
--- Lab2/src/server/Server.java
+++ Lab2/src/server/Server.java
@@ -1,6 +1,7 @@
 package src.server;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,11 +9,12 @@ public class Server {
     private final int port;
     private ServerSocket listener;
 
-    public Server(int port) {
+    public Server(String serverIp, int port) {
         this.port = port;
         try {
-            listener = new ServerSocket(port);
-//            listener.setSoTimeout(10000);
+            listener = new ServerSocket(port,50,InetAddress.getByName(serverIp));
+//            listener = new ServerSocket(port);
+            System.out.println(listener.getLocalSocketAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -20,14 +22,14 @@ public class Server {
 
     public void Start() {
         while (true) {
-            try{
-                System.out.println("port ["+port+"] is waiting for connection...");
-                Socket socket=listener.accept();
-                System.out.println("remote address is: "+socket.getRemoteSocketAddress());
-                System.out.println("local address is: "+socket.getLocalAddress()+":"+socket.getLocalPort());
+            try {
+                System.out.println("port [" + port + "] is waiting for connection...");
+                Socket socket = listener.accept();
+                System.out.println("remote address is: " + socket.getRemoteSocketAddress());
+                System.out.println("local address is: " + socket.getLocalAddress() + ":" + socket.getLocalPort());
                 socket.close();
                 System.out.println("connection has been closed.\n");
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
