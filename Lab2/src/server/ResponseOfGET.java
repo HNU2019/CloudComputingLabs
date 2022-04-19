3336956dff4c1e7aa336ee56cd839b932a6ba956
Hubert <1207820441@qq.com>
2022-04-19 21:12:16 +0800

    finish GET

--- Lab2/src/server/ResponseOfGET.java
+++ Lab2/src/server/ResponseOfGET.java
@@ -23,17 +23,53 @@ public class ResponseOfGET {
 
     /**
      * 发送给定文件的所有内容
+     * 会根据文件格式不同发送不同状态码
      *
      * @param filename 文件名
      */
     private void sendFileContent(String filename) {
+        File file = new File("static/" + filename);
+        //判断文件是否存在
+        if (!file.exists()) {
+            sendNotFound();
+            return;
+        }
+
         StringBuilder response = new StringBuilder();
         response.append(httpVersion);  //http版本
-        //判断文件是否存在
+        String type = "";
 
+        Pattern fileRegex = Pattern.compile(filePath);
+        Matcher m = fileRegex.matcher(url);
+        if (m.find()) {
+            String left = m.group(1);   // 文件名
+            String right = m.group(2);  // 文件格式
+//            System.out.println(left);
+//            System.out.println(right);
+            if (left.equals("403")) {
+                response.append(" 403 Forbidden\r\n");
+            } else if (left.equals("404")) {
+                response.append(" 404 Not Found\r\n");
+            } else if (left.equals("501")) {
+                response.append(" 501 Not Implemented\r\n");
+            } else if (left.equals("502")) {
+                response.append(" 502 Bad Gateway\r\n");
+            } else {
+                response.append(" 200 OK\r\n"); // 一般文件
+            }
+
+            if (right.equals("html")) type = "text/html";
+            else if (right.equals("js")) type = "text/javascript";
+            else if (right.equals("css")) type = "text/css";
+            else if (right.equals("json")) type = "application/json";
+        }
 
         try {
             writer.write(response.toString());
+            long fileLength = file.length();
+            writer.write(String.format("Content-type: %s\r\nContent-Length: %d\r\n\r\n", type, fileLength));
+            sendFileInBody(writer, file);
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,14 +79,34 @@ public class ResponseOfGET {
      * 发送/data/data.txt文件内容
      */
     private void apiCheck() {
+        StringBuilder response = new StringBuilder();
+        response.append(httpVersion + " 200 OK\r\n");
 
+        try {
+            writer.write(response.toString());
+            writer.write(String.format("Content-type: text/plain\r\nContent-Length: %d\r\n\r\n", data_txt.length));
+            writer.write(new String(data_txt, 0, data_txt.length));
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 发送/data/data.json文件内容
      */
     private void apiList() {
+        StringBuilder response = new StringBuilder();
+        response.append(httpVersion + " 200 OK\r\n");
 
+        try {
+            writer.write(response.toString());
+            writer.write(String.format("Content-type: text/json\r\nContent-Length: %d\r\n\r\n", data_json.length));
+            writer.write(new String(data_json, 0, data_json.length));
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -60,6 +116,53 @@ public class ResponseOfGET {
         StringBuilder response = new StringBuilder();
         response.append(httpVersion);  //http版本
         //判断条目是否存在
+        String data = new String(data_json);
+        StringBuilder ans = new StringBuilder("[]");
+//        System.out.println(data);
+        for (int i = 1; i < data.length() - 1; ) {
+            // 要保证每个循环开始时，i指向'{'
+            String _id = "", _name = "";
+            char c;
+            i += 6;  //id值的第一个字符
+            c = data.charAt(i);
+            while (c != ',') {
+                _id += c;
+                c = data.charAt(++i);
+            }
+            i += 9;  //指向name值的第一个字符
+            c = data.charAt(i);
+            while (c != '\"') {
+                _name += c;
+                c = data.charAt(++i);
+            }
+            i += 3;
+            // 开始比较
+//            System.out.printf("%s %s\n",_id,_name);
+            if ((id == null || _id.equals(id)) && (name == null || _name.equals(name))) {
+                ans.insert(ans.length()-1,String.format("{\"id\":%s,\"name\":\"%s\"}",_id,_name));
+            }
+        }
+        if (ans.length() == 2) {  // 没有匹配的
+            response.append(" 404 Not Found\r\n");
+            try {
+                writer.write(response.toString());
+                writer.write(String.format("Content-Type: text/html\r\nContent-length: %d\r\n\r\n", _404.length));
+                writer.write(new String(_404, 0, _404.length));   //读取_404数组并发送
+                writer.flush();  //最后刷新缓冲区
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            response.append(" 200 OK\r\n");
+            try{
+                writer.write(response.toString());
+                writer.write(String.format("Content-Type: application/json\r\nContent-length: %d\r\n\r\n", ans.length()));
+                writer.write(ans.toString());
+                writer.flush();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -67,13 +170,13 @@ public class ResponseOfGET {
      */
     private void sendNotFound() {
         StringBuilder response = new StringBuilder();
-        response.append(httpVersion);  //http版本
-        response.append(" 404 Not Found\r\n");
+        response.append(httpVersion + " 404 Not Found\r\n");  //http版本
+
         try {
             writer.write(response.toString());  //输出响应行
             int fileLength = _404.length;
             writer.write(String.format("Content-Type: text/html\r\nContent-length: %d\r\n\r\n", fileLength));
-            writer.write(new String(_404,0,_404.length));   //读取_404数组并发送
+            writer.write(new String(_404, 0, _404.length));   //读取_404数组并发送
             writer.flush();  //最后刷新缓冲区
         } catch (IOException e) {
             e.printStackTrace();
@@ -91,9 +194,6 @@ public class ResponseOfGET {
                 head = reader.readLine();
             }
             // 开始处理
-            //文件名的正则表达式
-
-
             if (url.charAt(url.length() - 1) == '/') { //请求的是目录
                 sendNotFound();
             } else if (url.equals("/api/check")) {
@@ -101,15 +201,15 @@ public class ResponseOfGET {
             } else if (url.equals("/api/list")) {
                 apiList();
             } else if (Pattern.matches(filePath, url)) {   //文件
-                System.out.println(url);
+                sendFileContent(url);
             } else if (Pattern.matches(search, url)) {  // search
-
                 Matcher m = searchRegex.matcher(url);
                 if (m.find()) {
                     String id = m.group(2);    //为空则为null
                     String name = m.group(4);  //为空则为null
 //                    System.out.println("id = " + id);
 //                    System.out.println("name = " + name);
+                    apiSearch(id, name);
                 }
             } else {
                 sendNotFound();
--- Lab2/src/server/Server.java
+++ Lab2/src/server/Server.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Server {
     private final int port;
     private ServerSocket listener;
-    protected final static String filePath = "/[^/\\:\\*\\?\"<>]*\\.[^/\\:\\*\\?\"<>]+"; //文件名的正则表达式
+    protected final static String filePath = "/([^/\\:\\*\\?\"<>]*)\\.([^/\\:\\*\\?\"<>]+)"; //文件名的正则表达式
     protected final static String search = "/api/search\\?(id=(\\d+))?&?(name=(.+))?";  //search的正则表达式
     protected static Pattern searchRegex;
     // 预先读入状态码文件
@@ -16,6 +16,8 @@ public class Server {
     protected static byte[] _404;
     protected static byte[] _501;
     protected static byte[] _502;
+    protected static byte[] data_txt; // data.txt
+    protected static byte[] data_json; // data.json
 
     public Server(String serverIp, int port) {
         this.port = port;
@@ -32,17 +34,21 @@ public class Server {
     }
 
     /**
-     * 服务器启动时将部分文件先读入到内存，403、404、501、502
+     * 服务器启动时将部分文件先读入到内存，403、404、501、502、data.txt
      */
     private void preReadFile() {
         File file403 = new File("static/403.html");
         File file404 = new File("static/404.html");
         File file501 = new File("static/501.html");
         File file502 = new File("static/502.html");
+        File dataTxt = new File("data/data.txt");
+        File dataJson = new File("data/data.json");
         _403 = new byte[(int) file403.length()];
         _404 = new byte[(int) file404.length()];
         _501 = new byte[(int) file501.length()];
         _502 = new byte[(int) file502.length()];
+        data_txt = new byte[(int) dataTxt.length()];
+        data_json = new byte[(int) dataJson.length()];
 
         try {
             // 读403
@@ -53,11 +59,21 @@ public class Server {
             fin = new BufferedInputStream(new FileInputStream(file404));
             fin.read(_404, 0, _404.length);
             fin.close();
+            // 读501
             fin = new BufferedInputStream(new FileInputStream(file501));
             fin.read(_501, 0, _501.length);
             fin.close();
+            // 读502
             fin = new BufferedInputStream(new FileInputStream(file502));
             fin.read(_502, 0, _502.length);
+            fin.close();
+            // 读data.txt
+            fin = new BufferedInputStream(new FileInputStream(dataTxt));
+            fin.read(data_txt, 0, data_txt.length);
+            fin.close();
+            // 读data.json
+            fin = new BufferedInputStream(new FileInputStream(dataJson));
+            fin.read(data_json, 0, data_json.length);
             fin.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
