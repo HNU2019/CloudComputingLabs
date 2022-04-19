9b9cdca83f2e7458b2a7b4df7438b8784631ad72
Hubert <1207820441@qq.com>
2022-04-19 15:48:39 +0800

    prefetch part of the file into memory

--- Lab2/src/server/Server.java
+++ Lab2/src/server/Server.java
@@ -3,7 +3,6 @@ package src.server;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.regex.Pattern;
 
 public class Server {
@@ -12,6 +11,11 @@ public class Server {
     protected final static String filePath = "/[^/\\:\\*\\?\"<>]*\\.[^/\\:\\*\\?\"<>]+"; //文件名的正则表达式
     protected final static String search = "/api/search\\?(id=(\\d+))?&?(name=(.+))?";  //search的正则表达式
     protected static Pattern searchRegex;
+    // 预先读入状态码文件
+    protected static byte[] _403;
+    protected static byte[] _404;
+    protected static byte[] _501;
+    protected static byte[] _502;
 
     public Server(String serverIp, int port) {
         this.port = port;
@@ -22,14 +26,52 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        searchRegex = Pattern.compile(search);
+        searchRegex = Pattern.compile(search);  // 静态编译search正则表达式
+
+        preReadFile();
+    }
+
+    /**
+     * 服务器启动时将部分文件先读入到内存，403、404、501、502
+     */
+    private void preReadFile() {
+        File file403 = new File("static/403.html");
+        File file404 = new File("static/404.html");
+        File file501 = new File("static/501.html");
+        File file502 = new File("static/502.html");
+        _403 = new byte[(int) file403.length()];
+        _404 = new byte[(int) file404.length()];
+        _501 = new byte[(int) file501.length()];
+        _502 = new byte[(int) file502.length()];
+
+        try {
+            // 读403
+            BufferedInputStream fin = new BufferedInputStream(new FileInputStream(file403));
+            fin.read(_403, 0, _403.length);
+            fin.close();
+            // 读404
+            fin = new BufferedInputStream(new FileInputStream(file404));
+            fin.read(_404, 0, _404.length);
+            fin.close();
+            fin = new BufferedInputStream(new FileInputStream(file501));
+            fin.read(_501, 0, _501.length);
+            fin.close();
+            fin = new BufferedInputStream(new FileInputStream(file502));
+            fin.read(_502, 0, _502.length);
+            fin.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void Start() {
         while (true) {
             try {
                 System.out.println("port [" + port + "] is waiting for connection...");
-                SocketThread connction=new SocketThread(listener.accept());
+                SocketThread connction = new SocketThread(listener.accept());
                 // 已连接到客户端
                 connction.run();
 
--- Lab2/src/server/SocketThread.java
+++ Lab2/src/server/SocketThread.java
@@ -5,19 +5,48 @@ import java.net.Socket;
 
 public class SocketThread implements Runnable {
     private Socket server;
+    private BufferedReader reader;
+    private BufferedWriter writer;
 
     public SocketThread(Socket socket) {
         server = socket;
         System.out.println("connected to remote address " + server.getRemoteSocketAddress());
         System.out.println("local address is " + server.getLocalAddress() + ":" + server.getLocalPort() + '\n');
+        try {
+            reader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 读取文件并作为body发送
+     * @param writer
+     * @param file
+     */
+    protected static void sendFileInBody(BufferedWriter writer, File file) {
+        try {
+            long fileLength = file.length();
+            BufferedInputStream fin = new BufferedInputStream(new FileInputStream(file));
+
+            byte[] bytes = new byte[8192];
+            int length = 0;
+            while (length < fileLength) {
+                length += fin.read(bytes, 0, bytes.length);
+                writer.write(new String(bytes,0,(length-1)%bytes.length+1));
+            }
+            fin.close();  //关闭文件输入流
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
-
             String str = reader.readLine();  //第一行是请求行，格式是"method URL http版本"
             System.out.println("/***** request *****/\n" + str);
             String[] request = str.split("[ ]+", 3);
@@ -26,7 +55,7 @@ public class SocketThread implements Runnable {
             String method = request[0];
 
             if (method.equals("GET")) {
-                ResponseOfGET doGet = new ResponseOfGET(server, request[1], request[2], reader, writer); // 不传入reader会引发阻塞
+                ResponseOfGET doGet = new ResponseOfGET(request[1], request[2], reader, writer); // 不传入reader会引发阻塞
                 doGet.response();
             } else if (method.equals("POST")) {
 
